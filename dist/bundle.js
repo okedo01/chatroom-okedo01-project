db4164071605e4d7e4436509140fee41ea6d5039
Eliah <eliahmwelangi01@gmail.com>
2024-12-18 00:50:12 +0300

    getting started with firestore

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,26 +7,43 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log(\"jdjdjdjd\");\r\n\n\n//# sourceURL=webpack://real-time-chatroom/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/forestore'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\r\n\r\n\r\nconst firebaseConfig = {\r\n    apiKey: \"AIzaSyA5HkoXk8Ak8uwn2d-7y75g9sldQs5kFIs\",\r\n    authDomain: \"real-time-chatroom-78101.firebaseapp.com\",\r\n    projectId: \"real-time-chatroom-78101\",\r\n    storageBucket: \"real-time-chatroom-78101.firebasestorage.app\",\r\n    messagingSenderId: \"1058210452892\",\r\n    appId: \"1:1058210452892:web:b0f24d273e6d80b6c46459\"\r\n  };\r\n\r\n//   initialize database app\r\n  Object(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(firebaseConfig)\r\n\r\n//   init services\r\nconst db = Object(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/forestore'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())()\r\n\r\n// collection ref\r\nconst colRef = Object(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/forestore'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(db, \"real-time-chatroom\")\n\n//# sourceURL=webpack://real-time-chatroom/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	__webpack_modules__["./src/index.js"](0, __webpack_exports__, __webpack_require__);
 /******/ 	
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("jdjdjdjd");
+import{ initilizeApp } from 'firebase/app'
+import { 
+    getFirestore, collection
+} from 'firebase/forestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA5HkoXk8Ak8uwn2d-7y75g9sldQs5kFIs",
+    authDomain: "real-time-chatroom-78101.firebaseapp.com",
+    projectId: "real-time-chatroom-78101",
+    storageBucket: "real-time-chatroom-78101.firebasestorage.app",
+    messagingSenderId: "1058210452892",
+    appId: "1:1058210452892:web:b0f24d273e6d80b6c46459"
+  };
+
+//   initialize database app
+  initilizeApp(firebaseConfig)
+
+//   init services
+const db = getFirestore()
+
+// collection ref
+const colRef = collection(db, "real-time-chatroom")
